7484dae33e8483d86f7602ed5c52f2c51e4fecae
Bo Yu-Chien Ning <bo.ning@yale.edu>
2021-01-30 21:38:43 +0100

    Add VBsparsePCA package

---
+++ R/foldednormalmean.R
@@ -0,0 +1,20 @@
+#' The function for obtaining the mean of a folded normal distribution
+#'
+#'
+#' This function calculates the mean of the folded normal distribution given its location and scale parameters.
+#'
+#'
+#' The mean of the folded normal distribution with location \eqn{\mu} and scale \eqn{\sigma^2} is
+#' \deqn{\sigma \sqrt{2/\pi} \exp(-\mu^2/(2\sigma^2)) + \mu (1-2\Phi(-\mu/\sigma))}.
+#'
+#'
+#'@param mean Location parameter of the folded normal distribution.
+#'@param var Scale parameter of the folded normal distribution.
+#'
+#'
+#'@return \item{foldednorm.mean}{The mean of the folded normal distribution of iterations to reach convergence.}
+#'
+#'
+foldednorm.mean <- function(mean, var) {
+  sqrt(var*2/pi) * exp(-mean^2/(2*var)) + mean*(1-2*pnorm(-mean/sqrt(var)))
+}
